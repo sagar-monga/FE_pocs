c15ebb43af69e16f25394804d56445f96e1849a3
Sagar Monga <sagar.monga@grapecity.com>
2024-07-31 17:29:20 +0530

    feat: update config for deployment

--- Learning React/05-password-generator/vite.config.ts
+++ Learning React/05-password-generator/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FE_pocs/Learning%20React/05-password-generator'
 })
