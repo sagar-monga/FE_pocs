8cce3cac624f666ed4d72606bfd8e8e1ec18f20c
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-01 13:54:35 +0530

    style: make elements fixed

--- Learning-React/05-password-generator/src/App.tsx
+++ Learning-React/05-password-generator/src/App.tsx
@@ -57,7 +57,7 @@ function App() {
 	return (
 		<div className="bg-black w-full h-screen flex justify-center items-center">
 			<div className="bg-gray-500 min-w-[800px] min-h-[400px] rounded-3xl shadow-lg shadow-gray-600 flex flex-col justify-center items-center">
-				<div className="flex min-w-[450px]">
+				<div className="flex min-w-[500px]">
 					<input
 						type="text"
 						className="bg-white h-full px-4 py-1 min-h-[40px] rounded-l-3xl flex-1"
@@ -78,7 +78,7 @@ function App() {
 						Regenerate
 					</button>
 				</div>
-				<div className="bg-green-500 min-w-[450px] min-h-[40px] flex justify-evenly items-center mt-5 rounded-full">
+				<div className="bg-green-500 min-w-[500px] min-h-[40px] flex justify-evenly items-center mt-5 rounded-full">
 					<div className="flex items-baseline">
 						<input
 							type="checkbox"
@@ -103,7 +103,7 @@ function App() {
 							max={20}
 							onChange={(event) => setLength(parseInt(event.target.value))}
 						/>
-						<span className="px-2">{`Length (${length})`}</span>
+						<span className="px-2 min-w-[100px]">{`Length (${length})`}</span>
 					</div>
 				</div>
 			</div>
