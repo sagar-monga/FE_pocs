e57cafd9cb15193b0b7df9071a6508dcd49b035a
Sagar Monga <sagar.monga3097@gmail.com>
2024-08-01 10:42:29 +0530

    chore: fix deployment

--- Learning React/05-password-generator/vite.config.ts
+++ Learning React/05-password-generator/vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FE_pocs/Learning%20React/05-password-generator'
 })
